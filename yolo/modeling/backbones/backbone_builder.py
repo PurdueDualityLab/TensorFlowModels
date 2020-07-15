4f5634c2c27067e64e1118960185e520bd97c7ec
vishnubanna <banna3vishnu@gmail.com>
2020-07-15 14:51:52 -0700

    builder init

--- yolo/modeling/backbones/backbone_builder.py
+++ yolo/modeling/backbones/backbone_builder.py
@@ -1,7 +1,11 @@
-import yolo.modeling.building_blocks as nn_blocks
+import tensorflow as tf 
+import tensorflow.keras as ks
 
-# (name, numberinblock, filters, kernal_size, downsample, route, output)
-darknet53 = {
+from yolo.modeling.building_blocks import *
+from yolo.modeling.backbones import *
+
+#temporary
+darknet53 = [
     ("DarkConv", 1, 32, 3, False, False, False), #1
     ("DarkRes", 1, 64, None, True, False, False), #3
     ("DarkRes", 1, 128, None, True, False, False), #3
@@ -12,4 +16,19 @@ darknet53 = {
     ("DarkRes", 7, 512, None, False, True, False), #14 route 61 last shortcut or last conv
     ("DarkRes", 1, 1024, None, True, False, False), #3
     ("DarkRes", 3, 1024, None, False, False, True), #6  #route  
-} #52 layers 
+ ] #52 layers 
+
+
+class YOLO_builder(ks.Model):
+    def __init__(self, **kwargs):
+        super(YOLO_builder, self).__init__(**kwargs)
+        return
+
+    def _build_struct(self):
+        endpoints = dict()
+        return
+    
+    def _connect_routes(self):
+        return
+    
+
