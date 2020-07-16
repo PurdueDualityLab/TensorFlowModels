1313796cc227c2ba1049192aaf259ffb5e024c92
vishnubanna <banna3vishnu@gmail.com>
2020-07-16 15:46:02 -0700

    yolo-loss

---
+++ yolo/modeling/loss_functions/yolo_loss.py
@@ -0,0 +1,14 @@
+import tensorflow as tf 
+import tensorflow.keras as ks 
+import tensorflow.keras import backend as K
+
+
+class Yolo_Loss(ks.losses.Loss):
+    def __init__(self, reduction, name=None):
+        pass
+
+    def call(self, y_true, y_pred):
+        pass 
+
+    def get_config(self):
+        pass
