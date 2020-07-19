de35cda3ec282f30379899b8de56efc22bce6468
anivegesana <anirudh.vegesana@gmail.com>
2020-07-19 11:50:11 -0400

    Add some testing code for YAML
    This may be useful in the future if we want to represent our models
    using YAML

---
+++ yolo/utils/dn_yaml.py
@@ -0,0 +1,88 @@
+"""
+Shortcuts for describing DarkNet models in YAML
+"""
+
+import copy
+import os
+import uuid
+from yolo.modeling.backbones import configs, general
+
+try:
+    import yaml
+except ImportError:
+    yaml = None
+
+try:
+    from importlib import resources as importlib_resources
+except:
+    import importlib_resources
+
+import tensorflow as tf
+
+if yaml is None:
+    def load_darknet_from_yaml(yaml_string, custom_objects=None):
+        raise ImportError('Requires yaml module installed (`pip install pyyaml`).')
+else:
+    class DarkNetYAMLLoader(yaml.SafeLoader):
+        "A special YAML loader with the apply to use the repeat constructor"
+
+        def repeat_constructor(self, tag_suffix: str, node):
+            """
+            The repeat constructor allows for the repetition of a layer muliple
+            times. It creates the same configuration and deep copies it so it can
+            appear multiple times without being routed in multiple places.
+            """
+            if ',' in tag_suffix:
+                times_, name = tag_suffix.split(',', 1)
+                times = int(times_)
+            else:
+                times = int(tag_suffix)
+                name = 'repeat_' + str(uuid.uuid1()).replace('-', '')
+            oneLayer = self.construct_mapping(node, deep=True)
+
+            def myCopyWithName(d: dict, i: int):
+                d = copy.deepcopy(d)
+                d['config']['name'] = d['config']['name'] + "_" + str(i)
+                return d
+
+            allLayers = {
+                'class_name': 'Sequential',
+                'config': {
+                    'layers': (
+                        [myCopyWithName(oneLayer, i) for i in range(times)]
+                        if 'name' in oneLayer['config'] else
+                        [copy.deepcopy(oneLayer) for i in range(times)]
+                    ),
+                    'name': name
+                }
+            }
+
+            return allLayers
+        def darknet(self, node):
+            # https://stackoverflow.com/a/9577670
+            filename = self.construct_scalar(node) + ".yml"
+            with importlib_resources.open_text(configs, filename) as f:
+                return yaml.load(f, self.__class__)
+        def construct_python_tuple(self, node):
+            return tuple(self.construct_sequence(node))
+
+    DarkNetYAMLLoader.add_constructor('tag:yaml.org,2002:python/tuple', DarkNetYAMLLoader.construct_python_tuple)
+    DarkNetYAMLLoader.add_multi_constructor('!repeat:', DarkNetYAMLLoader.repeat_constructor)
+    DarkNetYAMLLoader.add_constructor('!darknet', DarkNetYAMLLoader.darknet)
+
+    def load_darknet_from_yaml(yaml_string, custom_objects=None):
+        configDict = yaml.load(yaml_string, DarkNetYAMLLoader)
+        #import pprint
+        #pprint.pprint(configDict)
+        return tf.keras.layers.deserialize(configDict, custom_objects=custom_objects)
+
+
+if __name__ == '__main__':
+    from yolo.modeling.building_blocks import *
+    with open('yolo/modeling/backbones/configs/darknet_53.yml', mode='r') as file:
+        config = file.read()
+    loaded_model = load_darknet_from_yaml(config)
+    print(loaded_model)
+    with open('test.yml', mode='w') as file:
+        print(loaded_model.to_yaml(), file=file)
+    #tf.keras.utils.plot_model(loaded_model, to_file='model.png', show_shapes=False, show_layer_names=True,rankdir='TB', expand_nested=False, dpi=96)
