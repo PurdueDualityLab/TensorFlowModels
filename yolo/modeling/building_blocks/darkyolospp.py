12f6ff9ee9f867bc5a0d902235876ded3494e8cb
anivegesana <anirudh.vegesana@gmail.com>
2020-07-21 00:04:49 -0400

    Simple bug fixes for SPP block

--- yolo/modeling/building_blocks/darkyolospp.py
+++ yolo/modeling/building_blocks/darkyolospp.py
@@ -5,32 +5,32 @@ import tensorflow.keras as ks
 
 @ks.utils.register_keras_serializable(package='yolo')
 class darkyolospp(ks.layers.Layer):
-    def __init__(self, stride, sizes, **kwargs):
-        self.stride = stride
-        self.sizes = sizes
+    def __init__(self, sizes, **kwargs):
+        self._sizes = sizes
+        if len(sizes) == 0:
+            raise ValueError("More than one maxpool should be specified in SSP block")
         super().__init__(**kwargs)
         return
 
     def build(self, input_shape):
         maxpools = []
-        for size in sizes:
+        for size in self._sizes:
             maxpools.append(tf.keras.layers.MaxPool2D(
-                pool_size=self.size, strides=self.stride, padding='valid', data_format=None))
-        self.maxpools = maxpools
+                pool_size=(size, size), strides=(1, 1), padding='same', data_format=None))
+        self._maxpools = maxpools
         super().build(input_shape)
         return
 
     def call(self, inputs):
-        outputs = []
-        for maxpool in self.maxpools:
+        outputs = [inputs]
+        for maxpool in self._maxpools:
             outputs.append(maxpool(inputs))
         concat_output = ks.layers.concatenate(outputs)
         return concat_output
 
     def get_config(self):
         layer_config = {
-            "stride": self.stride,
-            "sizes": self.sizes
+            "sizes": self._sizes
         }
         layer_config.update(super().get_config())
         return layer_config
