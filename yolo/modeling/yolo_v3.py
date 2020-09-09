e6f3047ee23b76d8132292a61057dbd7a0d7a46a
anivegesana <anirudh.vegesana@gmail.com>
2020-09-09 17:15:00 -0400

    Remove future statements

--- yolo/modeling/yolo_v3.py
+++ yolo/modeling/yolo_v3.py
@@ -1,4 +1,3 @@
-#from __future__ import annotations
 
 import tensorflow as tf
 import tensorflow.keras as ks
--- yolo/utils/_darknet2tf/read_weights.py
+++ yolo/utils/_darknet2tf/read_weights.py
@@ -2,7 +2,6 @@
 This file contains the code to parse DarkNet weight files.
 """
 
-from __future__ import annotations
 
 import io
 import numpy as np
