21dcdbe5c8def22e23c76308245032aca69eaceb
vishnubanna <banna3vishnu@gmail.com>
2020-09-12 19:11:04 -0400

    lr change

--- yolo/modeling/building_blocks/_DarkTiny.py
+++ yolo/modeling/building_blocks/_DarkTiny.py
@@ -1,7 +1,7 @@
 """Contains common building blocks for yolo neural networks."""
 import tensorflow as tf
 import tensorflow.keras as ks
-from yolo.modeling.building_blocks import DarkConv
+import DarkConv
 
 
 @ks.utils.register_keras_serializable(package='yolo')
