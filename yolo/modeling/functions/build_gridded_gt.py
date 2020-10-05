88a26a2043bba6ddc50df1b2bc15c72ac0dfda6d
Jack LeCroy <3073035+jacklecroy@users.noreply.github.com>
2020-10-05 14:45:29 -0400

    Undo build_gridded_gt.py

--- yolo/modeling/functions/build_gridded_gt.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-from tensorflow.keras import backend as K
-
-
-@tf.function
-def build_gridded_gt_v1(y_true: tf.Tensor, mask: list[int], size: int,
-                        true_shape: tf.Tensor, use_tie_breaker: bool
-                        ) -> tf.Tensor:
-    pass
