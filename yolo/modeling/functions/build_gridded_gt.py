59a9edbe4623285d6621ad4e524fe8dfaa549a16
Jack LeCroy <3073035+jacklecroy@users.noreply.github.com>
2020-10-09 20:24:31 -0400

    Change build_gridded_gt params Required params for YOLO v1

--- yolo/modeling/functions/build_gridded_gt.py
+++ yolo/modeling/functions/build_gridded_gt.py
@@ -3,7 +3,17 @@ from tensorflow.keras import backend as K
 
 
 @tf.function
-def build_gridded_gt_v1(y_true: tf.Tensor, mask: list[int], size: int,
-                        true_shape: tf.Tensor, use_tie_breaker: bool
-                        ) -> tf.Tensor:
-    pass
+def build_gridded_gt_v1(y_true: tf.Tensor, size: int,
+                        true_shape: tf.Tensor) -> tf.Tensor:
+    """
+    Convert ground truth for use in loss functions.
+    Args:
+        y_true: tf.Tensor ground truth
+        size: dimensions of grid
+    """
+    batches = None  # figure out batches dimension
+    num_boxes = None  # figure out number of boxes
+    #gridded = tf.zeros()
+    for batch in range(batches):
+        for box in range(num_boxes):
+            pass
