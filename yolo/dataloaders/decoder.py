2bbfb110f185ab9dc39fdb30b8bd738a49354446
The-Indian-Chinna <54074879+The-Indian-Chinna@users.noreply.github.com>
2020-10-19 12:07:06 -0400

    FIXES

---
+++ yolo/dataloaders/decoder.py
@@ -0,0 +1,17 @@
+import abc
+
+
+class Decoder(object):
+  """Decodes the raw data into tensors."""
+
+  __metaclass__ = abc.ABCMeta
+
+  @abc.abstractmethod
+  def decode(self, serialized_example):
+    """Decodes the serialized example into tensors.
+    Args:
+      serialized_example: a serialized string tensor that encodes the data.
+    Returns:
+      decoded_tensors: a dict of Tensors.
+    """
+    pass
