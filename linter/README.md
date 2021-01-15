98be3db4cc38a55d7b3bb7e1591e2e899f998614
anivegesana <anirudh.vegesana@gmail.com>
2021-01-15 14:08:32 -0500

    Use autopep8 for more agressive linting and yapf for cleanup

--- linter/README.md
+++ linter/README.md
@@ -1,9 +1,6 @@
 # Custom linter for the TensorFlow team
 
-This linter is built on top of the YAPF Python formatter. These are the style faux pas that this hopes to correct.
-
-- bad-docstring-quotes (C0198)
-- inconsistent-quotes (W1405)
+This linter is built on top of the YAPF Python formatter. To see the style faux pas that this hopes to correct, run `python3 linter --help`.
 
 ## Try it out
 Run the following command to lint the yolo directory
--- linter/__main__.py
+++ linter/__main__.py
@@ -3,21 +3,31 @@ import glob
 import os
 import sys
 
-import yapf
-import yapf_contrib
+import autopep8
+from yapf.yapflib import yapf_api
+from yapf_contrib.fixers import fixers_api
 
-OPTS = ['quotes']
+OPTS = {'yapf': {'yapf', 'quotes'}, 'medium': {'autopep8', 'yapf', 'quotes'}}
 
 parser = argparse.ArgumentParser(
     description="""Lint the repo and correct mistakes that are not caught by YAPF.
 
 Options list:
+- yapf:
+  - general Google look
 - quotes:
-    - bad-docstring-quotes (C0198)
-    - inconsistent-quotes (W1405)
+  - bad-docstring-quotes (C0198)
+  - inconsistent-quotes (W1405)
 """,
     formatter_class=argparse.RawDescriptionHelpFormatter,
-    prefix_chars='-+')
+    prefix_chars='-+^')
+parser.add_argument(
+    '^x',
+    type=str,
+    dest='opts',
+    metavar='OPTIONS',
+    default='yapf',
+    help='option sets to default to')
 parser.add_argument(
     '-x',
     type=str,
@@ -31,6 +41,11 @@ parser.add_argument(
     metavar='OPTIONS',
     help='enable linting options')
 parser.add_argument(
+    '--autopep8-agression',
+    type=int,
+    help='aggressiveness of autopep8',
+    default=2)
+parser.add_argument(
     'files',
     nargs='+',
     help='if a directory is specified, all Python files inside are used')
@@ -43,17 +58,39 @@ for i, file in enumerate(files):
   elif not os.path.exists(file):
     raise FileNotFoundError(file)
 
-opts = OPTS.copy()
+opts = set()
+for optset in args.opts.split(','):
+  opts.update(OPTS[optset])
 if args.enopts is not None:
-  remopts = args.enopts.split(',')
-  for opt in remopts:
-    opts.append(opt)
+  opts.update(args.enopts.split(','))
 if args.noopts is not None:
-  remopts = args.noopts.split(',')
-  for opt in remopts:
-    opts.remove(opt)
+  opts.difference_update(args.noopts.split(','))
 
 # The real linter starts here
-if 'quotes' in opts:
-  yapf_contrib.enabled_fixers.append('quotes')
-yapf.main([sys.argv[0], '-i', *files])
+if len(opts) != 0:
+  for file in files:
+    changed = False
+    with open(file) as src_file:
+      src = src_file.read()
+
+    if 'autopep8' in opts:
+      old_src = src
+      src = autopep8.fix_code(
+          src, options={'aggressive': args.autopep8_agression})
+      changed |= (old_src != src)
+      del old_src
+
+    if 'quotes' in opts:
+      old_src = src
+      src = fixers_api.Pre2to3FixerRun(src, {'fixers': ['quotes']})
+      changed |= (old_src != src)
+      del old_src
+
+    if 'yapf' in opts:
+      src, _changed = yapf_api.FormatCode(src, file, 'yapf')
+      changed |= _changed
+
+    if changed:
+      with open(file, 'w') as src_file:
+        src_file.write(src)
+    print(changed)
