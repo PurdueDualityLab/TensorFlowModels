f9a0cc875da43ccf74adf6eadbe01ba3fff0951b
vishnubanna <banna3vishnu@gmail.com>
2021-01-20 17:08:20 -0500

    updated testing

--- official/vision/beta/tasks/image_classification.py
+++ official/vision/beta/tasks/image_classification.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 # ==============================================================================
 """Image classification task definition."""
+from absl import logging
 import tensorflow as tf
 from official.core import base_task
 from official.core import input_reader
@@ -21,6 +22,7 @@ from official.core import task_factory
 from official.modeling import tf_utils
 from official.vision.beta.configs import image_classification as exp_cfg
 from official.vision.beta.dataloaders import classification_input
+from official.vision.beta.dataloaders import dataset_fn
 from official.vision.beta.modeling import factory
 
 
@@ -46,6 +48,30 @@ class ImageClassificationTask(base_task.Task):
         l2_regularizer=l2_regularizer)
     return model
 
+  def initialize(self, model: tf.keras.Model):
+    """Loading pretrained checkpoint."""
+    if not self.task_config.init_checkpoint:
+      return
+
+    ckpt_dir_or_file = self.task_config.init_checkpoint
+    if tf.io.gfile.isdir(ckpt_dir_or_file):
+      ckpt_dir_or_file = tf.train.latest_checkpoint(ckpt_dir_or_file)
+
+    # Restoring checkpoint.
+    if self.task_config.init_checkpoint_modules == 'all':
+      ckpt = tf.train.Checkpoint(**model.checkpoint_items)
+      status = ckpt.restore(ckpt_dir_or_file)
+      status.assert_consumed()
+    elif self.task_config.init_checkpoint_modules == 'backbone':
+      ckpt = tf.train.Checkpoint(backbone=model.backbone)
+      status = ckpt.restore(ckpt_dir_or_file)
+      status.expect_partial().assert_existing_objects_matched()
+    else:
+      assert "Only 'all' or 'backbone' can be used to initialize the model."
+
+    logging.info('Finished loading pretrained checkpoint from %s',
+                 ckpt_dir_or_file)
+
   def build_inputs(self, params, input_context=None):
     """Builds classification input."""
 
@@ -56,11 +82,12 @@ class ImageClassificationTask(base_task.Task):
     parser = classification_input.Parser(
         output_size=input_size[:2],
         num_classes=num_classes,
+        aug_policy=params.aug_policy,
         dtype=params.dtype)
 
     reader = input_reader.InputReader(
         params,
-        dataset_fn=tf.data.TFRecordDataset,
+        dataset_fn=dataset_fn.pick_dataset_fn(params.file_type),
         decoder_fn=decoder.decode,
         parser_fn=parser.parse_fn(params.is_training))
 
@@ -98,15 +125,17 @@ class ImageClassificationTask(base_task.Task):
 
   def build_metrics(self, training=True):
     """Gets streaming metrics for training/validation."""
+    k = self.task_config.evaluation.top_k
     if self.task_config.losses.one_hot:
       metrics = [
           tf.keras.metrics.CategoricalAccuracy(name='accuracy'),
-          tf.keras.metrics.TopKCategoricalAccuracy(k=5, name='top_5_accuracy')]
+          tf.keras.metrics.TopKCategoricalAccuracy(
+              k=k, name='top_{}_accuracy'.format(k))]
     else:
       metrics = [
           tf.keras.metrics.SparseCategoricalAccuracy(name='accuracy'),
           tf.keras.metrics.SparseTopKCategoricalAccuracy(
-              k=5, name='top_5_accuracy')]
+              k=k, name='top_{}_accuracy'.format(k))]
     return metrics
 
   def train_step(self, inputs, model, optimizer, metrics=None):
@@ -143,7 +172,7 @@ class ImageClassificationTask(base_task.Task):
       # For mixed_precision policy, when LossScaleOptimizer is used, loss is
       # scaled for numerical stability.
       if isinstance(
-          optimizer, tf.keras.mixed_precision.experimental.LossScaleOptimizer):
+          optimizer, tf.keras.mixed_precision.LossScaleOptimizer):
         scaled_loss = optimizer.get_scaled_loss(scaled_loss)
 
     tvars = model.trainable_variables
@@ -151,13 +180,8 @@ class ImageClassificationTask(base_task.Task):
     # Scales back gradient before apply_gradients when LossScaleOptimizer is
     # used.
     if isinstance(
-        optimizer, tf.keras.mixed_precision.experimental.LossScaleOptimizer):
+        optimizer, tf.keras.mixed_precision.LossScaleOptimizer):
       grads = optimizer.get_unscaled_gradients(grads)
-
-    # Apply gradient clipping.
-    if self.task_config.gradient_clip_norm > 0:
-      grads, _ = tf.clip_by_global_norm(
-          grads, self.task_config.gradient_clip_norm)
     optimizer.apply_gradients(list(zip(grads, tvars)))
 
     logs = {self.loss: loss}
---
+++ yolo/test.py
@@ -0,0 +1,38 @@
+from yolo.utils.run_utils import prep_gpu
+from yolo.configs import yolo as exp_cfg
+from yolo.tasks.yolo import YoloTask
+import yolo.utils.export.tensor_rt as trt
+import tensorflow as tf 
+
+prep_gpu()
+
+from tensorflow.keras.mixed_precision import experimental as mixed_precision
+mixed_precision.set_policy("mixed_float16")
+# mixed_precision.set_policy("float32")
+
+config = exp_cfg.YoloTask(model=exp_cfg.Yolo(base='v4', 
+                        min_level=3, 
+                        norm_activation = exp_cfg.common.NormActivation(activation="mish"), 
+                        #norm_activation = exp_cfg.common.NormActivation(activation="leaky"), 
+                        #_boxes = ['(10, 14)', '(23, 27)', '(37, 58)', '(81, 82)', '(135, 169)', '(344, 319)'],
+                        #_boxes = ["(10, 13)", "(16, 30)", "(33, 23)","(30, 61)", "(62, 45)", "(59, 119)","(116, 90)", "(156, 198)", "(373, 326)"],
+                        _boxes = ['(12, 16)', '(19, 36)', '(40, 28)', '(36, 75)','(76, 55)', '(72, 146)', '(142, 110)', '(192, 243)','(459, 401)'],
+                        filter = exp_cfg.YoloLossLayer(use_nms=False)),
+                        load_darknet_weights = False,
+                        darknet_load_decoder = False) 
+
+# config = exp_cfg.YoloTask(model=exp_cfg.Yolo(base='v3', 
+#                     min_level=3, 
+#                     #norm_activation = exp_cfg.common.NormActivation(activation="mish"), 
+#                     norm_activation = exp_cfg.common.NormActivation(activation="leaky"), 
+#                     #_boxes = ['(10, 14)', '(23, 27)', '(37, 58)', '(81, 82)', '(135, 169)', '(344, 319)'],
+#                     #_boxes = ["(10, 13)", "(16, 30)", "(33, 23)","(30, 61)", "(62, 45)", "(59, 119)","(116, 90)", "(156, 198)", "(373, 326)"],
+#                     _boxes = ['(12, 16)', '(19, 36)', '(40, 28)', '(36, 75)','(76, 55)', '(72, 146)', '(142, 110)', '(192, 243)','(459, 401)'],
+#                     filter = exp_cfg.YoloLossLayer(use_nms=False)
+#                     ))   
+task = YoloTask(config)
+model = task.build_model()
+task.initialize(model)
+
+
+model(tf.ones((1, 416, 416, 3), tf.float32))
