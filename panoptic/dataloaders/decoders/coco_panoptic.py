13fecfa0ddd6210c4d3e461076fb180724de3a33
vishnubanna <banna3vishnu@gmail.com>
2021-01-26 13:50:03 -0500

    decoder

--- panoptic/dataloaders/decoders/coco_panoptic.py
+++ panoptic/dataloaders/decoders/coco_panoptic.py
@@ -1,15 +1,16 @@
 import tensorflow_datasets as tfds 
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import utils.demos.utils as utils
 
 from panoptic.dataloaders.decoders import tfds_panoptic_coco_decoder
 
 path = "/media/vbanna/DATA_SHARE/tfds"
 dataset = "coco/2017_panoptic"
 val = tfds.load(dataset, data_dir = path, split = "validation")
+drawer = utils.DrawBoxes(classes = 133, display_names=False)
 
 decoder = tfds_panoptic_coco_decoder.MSCOCODecoder(include_mask=True)
-print(val)
 val = val.map(decoder.decode)
 
 lim = 10
@@ -17,7 +18,12 @@ for i, sample in enumerate(val):
   fig, axe = plt.subplots(1, 3)
   axe[0].imshow(sample["groundtruth_semantic_mask"])
   axe[1].imshow(sample["groundtruth_instance_id"] % 256)
-  axe[2].imshow(sample["image"])
+
+  image = sample["image"]/255
+  
+  results = {"classes": sample["groundtruth_classes"], "bbox":sample['groundtruth_boxes']}
+  image = drawer(image, results)
+  axe[2].imshow(image)
   plt.show()
   if i > (lim + 1):
     break
