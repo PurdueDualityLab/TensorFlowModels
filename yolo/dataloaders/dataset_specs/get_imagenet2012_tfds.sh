42b7b57f39532b7c3b5b0938ea0a9eb4e80454b9
anivegesana <anirudh.vegesana@gmail.com>
2021-02-04 10:14:28 -0500

    Resume downloads in ImageNet downloader

--- yolo/dataloaders/dataset_specs/get_imagenet2012_tfds.sh
+++ yolo/dataloaders/dataset_specs/get_imagenet2012_tfds.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# email me: vbanna@purdue.edu if there are any issues. 
+# email me: vbanna@purdue.edu if there are any issues.
 # register your account to download imagenet here http://image-net.org/signup.php?next=download-images
 
-# run instructions: 
+# run instructions:
 # > chmod +x get_imagenet2012_tfds.sh
 # > sudo ./get_imagenet2012_tfds.sh
 
@@ -12,11 +12,11 @@ ID=dd31405981ef5f776aa17412e1f0c112 #replace this with the ID you recieved when 
 # axel is a library that uses threading to improve download speeds. I highly suggest you download it. This script should handle that
 INSTALL_AXEL=1 # if axel is not installed, then install it
 AXEL=1 # use axel to load the imagenet zip files over wget. this will reduce download times by a factor of at least 2x
-NUM_PARA=4 
+NUM_PARA=4
 CHECK_AXEL=$(command -v axel)
 
 #default
-PATH_TO_TFDS=/media/vbanna/DATA_SHARE/tfds
+PATH_TO_TFDS=~/tensorflow_datasets
 DOWNLOADS_DOWNLOAD_DIR=$PATH_TO_TFDS/downloads
 MANUAL_DOWNLOAD_DIR=$PATH_TO_TFDS/downloads/manual
 
@@ -24,6 +24,21 @@ TRAIN="http://www.image-net.org/challenges/LSVRC/2012/$ID/ILSVRC2012_img_train.t
 TEST="http://www.image-net.org/challenges/LSVRC/2012/$ID/ILSVRC2012_img_test_v10102019.tar"
 VAL="http://www.image-net.org/challenges/LSVRC/2012/$ID/ILSVRC2012_img_val.tar"
 TEST_PATCH="https://drive.google.com/u/0/uc?id=16RYnHpVOW0XKCsn3G3S9GTHUyoV2-4WX&export=download"
+
+# from https://stackoverflow.com/a/13272369
+function custom_axel() {
+    local file_thingy="$1"
+    local url="$2"
+    if [ ! -e "$file_thingy" ]; then
+        echo "file not found, downloading: $file_thingy"
+        axel -avn$NUM_PARA "$url" -o "$file_thingy"
+    elif [ -e "${file_thingy}.st" ]; then
+        echo "found partial downloaf, resuming: $file_thingy"
+        axel -avn$NUM_PARA "$url" -o "$file_thingy"
+    else
+        echo "already have the file, skipped: $file_thingy"
+    fi
+}
 
 echo "Links to Imagenet"
 echo "train: $TRAIN"
@@ -46,43 +61,35 @@ mkdir -p $MANUAL_DOWNLOAD_DIR
 cd $MANUAL_DOWNLOAD_DIR
 pwd
 
-if [ ! -e $MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_train.tar ]
+if [ $AXEL -eq 1 ]
 then
-    if [ $AXEL -eq 1 ]
-    then
-        echo "downloading using AXEL"
-        axel -n $NUM_PARA -a --output=$MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_train.tar $TRAIN
-    else
-        wget $TRAIN -O $MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_train.tar
-    fi 
+    echo "downloading using AXEL"
+    custom_axel $MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_train.tar $TRAIN
 else
-    echo "Train tar file exists already"
+    wget -c $TRAIN -O $MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_train.tar
 fi
 
-if [ ! -e $MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_val.tar ]
+if [ $AXEL -eq 1 ]
 then
-    if [ $AXEL -eq 1 ]
-    then
-        echo "downloading val using AXEL"
-        axel -n $NUM_PARA -a --output=$MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_val.tar $VAL
-    else
-        wget $VAL -O $MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_val.tar
-    fi 
+    echo "downloading val using AXEL"
+    custom_axel $MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_val.tar $VAL
 else
-    echo "Val tar file exists already"
+    wget -c $VAL -O $MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_val.tar
 fi
 
-if [ ! -e $MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_test_v10102019.tar ]  && [ ! -e $MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_test.tar ]
+if [ $AXEL -eq 1 ]
 then
-    if [ $AXEL -eq 1 ]
-    then
-        echo "downloading test using AXEL"
-        axel -n $NUM_PARA -a --output=$MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_test.tar $TEST
-    else
-        wget $TEST -O $MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_test.tar
-    fi 
+    echo "downloading test using AXEL"
+    axel -n $NUM_PARA -a --output=$MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_test.tar $TEST
 else
-    echo "Test tar file exists already"
+    wget -c $TEST -O $MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_test.tar
+fi
+
+if [ ! -e $MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_test_v10102019.tar ]
+then
+    wget $VAL -O $MANUAL_DOWNLOAD_DIR/ILSVRC2012_img_test_v10102019.tar
+else
+    echo "Test patch tar file exists already"
 fi
 
 python3 -m tensorflow_datasets.scripts.download_and_prepare --datasets=imagenet2012 --data_dir=$PATH_TO_TFDS --download_dir=$DOWNLOADS_DOWNLOAD_DIR --manual_dir=$MANUAL_DOWNLOAD_DIR
