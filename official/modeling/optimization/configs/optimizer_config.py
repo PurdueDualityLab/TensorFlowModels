18b13bcc8a9496cef7e8e0ef2508ccf72ee6dfe9
vishnubanna <banna3vishnu@gmail.com>
2021-02-16 01:35:13 -0500

    Gradient Accumulated SGD optimizer for Large Batch Size simulation

--- official/modeling/optimization/configs/optimizer_config.py
+++ official/modeling/optimization/configs/optimizer_config.py
@@ -55,6 +55,24 @@ class SGDConfig(BaseOptimizerConfig):
 
 
 @dataclasses.dataclass
+class SGDAccumConfig(BaseOptimizerConfig):
+  """Configuration for SGD optimizer.
+
+  The attributes for this class matches the arguments of tf.keras.optimizer.SGD.
+
+  Attributes:
+    name: name of the optimizer.
+    decay: decay rate for SGD optimizer.
+    nesterov: nesterov for SGD optimizer.
+    momentum: momentum for SGD optimizer.
+  """
+  name: str = "SGD"
+  accumulation_steps: int = 1
+  decay: float = 0.0
+  momentum: float = 0.0
+
+
+@dataclasses.dataclass
 class RMSPropConfig(BaseOptimizerConfig):
   """Configuration for RMSProp optimizer.
 
