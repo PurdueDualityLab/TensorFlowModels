60090a21d6fb2961ab33479ef29e790af04d9231
anivegesana <anirudh.vegesana@gmail.com>
2021-02-27 13:24:48 -0500

    TPU setup menu

---
+++ utils/tpu/create_kconfig.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import os
+import sys
+import glob
+
+dir = os.path.dirname(__file__)
+dest = os.path.join(dir, 'Kconfig')
+template = os.path.join(dir, 'Kconfig.template')
+creds = os.path.join(dir, 'creds', '*.json')
+
+if sys.stdin.isatty():
+  credList = glob.glob(creds)
+  if credList:
+    for i, credFileName in enumerate(credList, 1):
+      print(f"[{i}] {os.path.basename(credFileName)}")
+    i = int(input("Select a credential file: "))
+    AUTH_KEY = credList[i - 1]
+  else:
+    print("No credential files found. Please make an IAM key and save it to " +
+        "the creds folder")
+    exit(1)
+else:
+  AUTH_KEY = next(glob.iglob(creds))
+
+with open(template) as tfile, open(dest, 'w') as dfile:
+  dfile.write(tfile.read().format(AUTH_KEY=repr(AUTH_KEY)))
---
+++ utils/tpu/show_menu.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+import ast
+import configparser
+import contextlib
+import curses
+import json
+from distutils.util import strtobool
+import subprocess
+import sys
+import os
+
+USE_GUI = False
+
+if USE_GUI:
+  import guiconfig
+  menuconfig = guiconfig
+else:
+  import menuconfig
+import kconfiglib
+
+#menuconfig.menuconfig(kconfiglib.standard_kconfig(''))
+
+kconf = kconfiglib.standard_kconfig('')
+status = ''
+
+def _quit(msg=None):
+  global status
+  guiconfig._root.destroy()
+  if msg:
+    status = msg
+
+if USE_GUI:
+  guiconfig._quit = _quit
+  guiconfig.menuconfig(kconf)
+else:
+  with contextlib.redirect_stdout(sys.stderr):
+    menuconfig._kconf = kconf
+    menuconfig._conf_filename = menuconfig.standard_config_filename()
+    menuconfig._conf_changed = menuconfig._load_config()
+    menuconfig._minconf_filename = 'config'
+    menuconfig._show_all = False
+    kconf.warn = False
+    os.environ.setdefault("ESCDELAY", "0")
+  status = curses.wrapper(menuconfig._menuconfig)
+
+if 'was not saved' in status:
+  print('Changes not saved')
+  exit(1)
+
+if not menuconfig._conf_changed:
+  if input('Spin up the same instance as last time? [yN] ').lower() != 'y':
+    print('No new instances')
+    exit(1)
+
+my_env = {}
+parser = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
+def defaultSect(fp): yield '[DEFAULT]\n'; yield from fp
+settingsFile = '.config'
+if os.path.isfile(settingsFile):
+    with open(settingsFile) as stream:
+        parser.read_file(defaultSect(stream))
+        for k, v in parser["DEFAULT"].items():
+            my_env.setdefault(k.upper(), v)
+
+
+CPU = strtobool(my_env.get('CONFIG_VM', 'n'))
+TPU = strtobool(my_env.get('CONFIG_TPU', 'n'))
+AUTH_KEY = ast.literal_eval(my_env['CONFIG_AUTH_KEY'])
+VM_NAME = ast.literal_eval(my_env.get('CONFIG_VM_NAME', '""'))
+TPU_NAME = ast.literal_eval(my_env.get('CONFIG_TPU_NAME', '""'))
+
+if my_env.get('CONFIG_TPU_ZONE__US_CENTRAL1_F', 'n') == 'y':
+  TPU_ZONE = "us-central1-f"
+elif my_env.get('CONFIG_TPU_ZONE__US_CENTRAL1_A', 'n') == 'y':
+  TPU_ZONE = "us-central1-a"
+elif my_env.get('CONFIG_TPU_ZONE__EUROPE_WEST4_A', 'n') == 'y':
+  TPU_ZONE = "europe-west4-a"
+else:
+  raise ValueError("Unknown Zone")
+
+if CPU:
+  if my_env.get('CONFIG_CPU_SIZE__E2_STANDARD_16', 'n') == 'y':
+    CPU_SIZE = "e2-standard-16"
+  else:
+    raise ValueError("Unknown CPU")
+
+if TPU:
+  if my_env.get('CONFIG_TPU_SIZE__V2_256', 'n') == 'y':
+    TPU_SIZE = "v2-256"
+  elif my_env.get('CONFIG_TPU_SIZE__V2_8', 'n') == 'y':
+    TPU_SIZE = "v2-8"
+  elif my_env.get('CONFITPU_SIZE__V3_8', 'n') == 'y':
+    TPU_SIZE = "v3-8"
+  else:
+    raise ValueError("Unknown TPU")
+
+
+with open(AUTH_KEY) as file:
+  PROJECT_NAME = json.load(file)['project_id']
+
+def new_cpu_tpu():
+  proc = subprocess.Popen(['ctpu', 'up', f'--tpu-size={TPU_SIZE}', f'--name={VM_NAME}', f'--project={PROJECT_NAME}', f'--zone={TPU_ZONE}', '--disk-size-gb=50', f'--machine-type={CPU_SIZE}', '--tf-version=2.4.1'], stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr)
+  proc.communicate()
+
+def new_cpu():
+  proc = subprocess.Popen(['ctpu', 'up', '--vm-only', f'--name={VM_NAME}', f'--project={PROJECT_NAME}', f'--zone={TPU_ZONE}', '--disk-size-gb=50', f'--machine-type={CPU_SIZE}', '--tf-version=2.4.1'], stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr)
+  proc.communicate()
+
+def new_tpu():
+  proc = subprocess.Popen(['ctpu', 'up', '--tpu-only', f'--tpu-size={TPU_SIZE}', f'--name={TPU_NAME}', f'--project={PROJECT_NAME}', f'--zone={TPU_ZONE}', '--tf-version=2.4.1'], stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr)
+  proc.communicate()
+
+if CPU:
+  if TPU:
+    if TPU_NAME == VM_NAME:
+      new_cpu_tpu()
+    else:
+      new_cpu()
+      new_tpu()
+  else:
+    new_cpu()
+else:
+  if TPU:
+    new_tpu()
+  else:
+    print("Nothing to configure")
+    exit(1)
