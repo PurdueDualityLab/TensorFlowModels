bee19d7d01e3752d17c9f03d5b1e502b71f63691
vishnubanna <banna3vishnu@gmail.com>
2021-03-02 00:23:44 -0500

    model

--- yolo/configs/yolo.py
+++ yolo/configs/yolo.py
@@ -187,8 +187,8 @@ class DataDecoder(hyperparams.OneOfConfig):
 class DataConfig(cfg.DataConfig):
   """Input config for training."""
   input_path: str = ''
-  tfds_name: str = #'coco'
-  tfds_split: str = #'train'
+  tfds_name: str = None #'coco'
+  tfds_split: str = None #'train'
   global_batch_size: int = 32
   is_training: bool = True
   dtype: str = 'float16'
