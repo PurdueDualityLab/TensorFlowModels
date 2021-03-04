de48da6599c9b7aa266e3ee6f4b9f58a8cc97bf2
vishnubanna <banna3vishnu@gmail.com>
2021-03-03 23:18:18 -0500

    nan

--- yolo/ops/box_ops.py
+++ yolo/ops/box_ops.py
@@ -46,14 +46,11 @@ def xcycwh_to_yxyx(box: tf.Tensor, split_min_max: bool = False):
 
 
 # IOU
-def rm_nan_inf(x, val=0.0):
-  x = tf.where(
-      tf.math.logical_or(tf.math.is_nan(x), tf.math.is_inf(x)),
-      tf.cast(val, dtype=x.dtype), x)
+def rm_nan_inf(x, val = 0.0):
+  x = tf.where(tf.math.logical_or(tf.math.is_nan(x), tf.math.is_inf(x)), tf.cast(val, dtype=x.dtype), x)
   return x
 
-
-def rm_nan(x, val=0.0):
+def rm_nan(x, val = 0.0):
   x = tf.where(tf.math.is_nan(x), tf.cast(val, dtype=x.dtype), x)
   return x
 
@@ -87,9 +84,10 @@ def compute_iou(box1, box2, yxyx=False):
     box2_area = tf.math.abs(tf.reduce_prod(b2ma - b2mi, axis=-1))
     union = box1_area + box2_area - intersection
 
-    iou = tf.math.divide_no_nan(intersection, union)
-    #iou = rm_nan_inf(iou)
+    iou = tf.math.divide(intersection, union + K.epsilon())  
+    iou = rm_nan(iou)
 
+    # iou = tf.math.divide_no_nan(intersection, union)
     iou = tf.clip_by_value(iou, clip_value_min=0.0, clip_value_max=1.0)
   return iou
 
@@ -125,8 +123,8 @@ def compute_giou(box1, box2, yxyx=False):
     union = box1_area + box2_area - intersection
 
     # iou = tf.math.divide(intersection, union + K.epsilon())
+    # iou = rm_nan(iou)
     iou = tf.math.divide_no_nan(intersection, union)
-    #iou = rm_nan_inf(iou)
     iou = tf.clip_by_value(iou, clip_value_min=0.0, clip_value_max=1.0)
 
     # find the smallest box to encompase both box1 and box2
@@ -135,10 +133,9 @@ def compute_giou(box1, box2, yxyx=False):
     c = tf.math.abs(tf.reduce_prod(c_mins - c_maxes, axis=-1))
 
     # compute giou
-    # regularization = tf.math.divide((c - union), c + K.epsilon())
+    #regularization = tf.math.divide((c - union), c + K.epsilon())
     regularization = tf.math.divide_no_nan((c - union), c)
     giou = iou - regularization
-    #giou = rm_nan_inf(giou)
   return iou, giou
 
 
@@ -178,8 +175,9 @@ def compute_diou(box1, box2, yxyx=False):
     box2_area = tf.math.abs(tf.reduce_prod(b2ma - b2mi, axis=-1))
     union = box1_area + box2_area - intersection
 
-    iou = tf.math.divide_no_nan(intersection, union + K.epsilon())
-    #iou = rm_nan_inf(iou)
+    iou = tf.math.divide(intersection, union + K.epsilon())
+    iou = rm_nan(iou)
+    #iou = tf.math.divide_no_nan(intersection, union)
     iou = tf.clip_by_value(iou, clip_value_min=0.0, clip_value_max=1.0)
 
     # compute max diagnal of the smallest enclosing box
@@ -188,11 +186,10 @@ def compute_diou(box1, box2, yxyx=False):
 
     diag_dist = tf.reduce_sum((c_maxes - c_mins)**2, axis=-1)
 
-    #regularization = tf.math.divide(dist, diag_dist + K.epsilon())
-    # regularization = rm_nan(regularization)
-    regularization = tf.math.divide_no_nan(dist, diag_dist)
+    regularization = tf.math.divide(dist, diag_dist + K.epsilon())
+    regularization = rm_nan(regularization)
+    #regularization = tf.math.divide_no_nan(dist, diag_dist)
     diou = iou - regularization
-    #diou = rm_nan_inf(diou)
   return iou, diou
 
 
@@ -215,14 +212,40 @@ def compute_ciou(box1, box2, yxyx=False):
       box1 = yxyx_to_xcycwh(box1)
       box2 = yxyx_to_xcycwh(box2)
 
+    # computer aspect ratio consistency
+    # arcterm = tf.square(
+    #     tf.math.atan(tf.math.divide_no_nan(box1[..., 2], box1[..., 3])) -
+    #     tf.math.atan(tf.math.divide_no_nan(box2[..., 2], box2[..., 3])))
+    
     arcterm = tf.square(
-        tf.math.atan(tf.math.divide_no_nan(box1[..., 2], box1[..., 3])) -
-        tf.math.atan(tf.math.divide_no_nan(box2[..., 2], box2[..., 3])))
+        tf.math.atan(tf.math.divide(box1[..., 2], box1[..., 3])) -
+        tf.math.atan(tf.math.divide(box2[..., 2], box2[..., 3])))
     v = 4 * arcterm / (math.pi**2)
-
+    v = rm_nan(v)
     # compute IOU regularization
     # a = tf.math.divide(v, ((1 - iou) + v) + K.epsilon())
     a = tf.math.divide_no_nan(v, ((1 - iou) + v))
     ciou = diou - (v * a)
-    #ciou = rm_nan_inf(ciou)
   return iou, ciou
+
+
+def aggregated_comparitive_iou(boxes1, boxes2=None, iou_type=0, xyxy=True):
+  k = tf.shape(boxes1)[-2]
+
+  boxes1 = tf.expand_dims(boxes1, axis=-2)
+  boxes1 = tf.tile(boxes1, [1, 1, k, 1])
+
+  if boxes2 is not None:
+    boxes2 = tf.expand_dims(boxes2, axis=-2)
+    boxes2 = tf.tile(boxes2, [1, 1, k, 1])
+    boxes2 = tf.transpose(boxes2, perm=(0, 2, 1, 3))
+  else:
+    boxes2 = tf.transpose(boxes1, perm=(0, 2, 1, 3))
+
+  if iou_type == 0: #diou
+    _, iou = compute_diou(boxes1, boxes2, yxyx=True)
+  elif iou_type == 1: #giou
+    _, iou = compute_giou(boxes1, boxes2, yxyx=True)
+  else:
+    iou = box_ops.compute_iou(boxes1, boxes2, yxyx=True)
+  return iou
