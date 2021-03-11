ce61a7c4654091afe6f35b0cb8d99f22ce8d1b4e
David Li <davidli2881@gmail.com>
2021-03-11 15:21:11 -0500

    demo working, adding docs later

--- centernet/modeling/layers/detection_generator.py
+++ centernet/modeling/layers/detection_generator.py
@@ -1,23 +1,28 @@
 from tensorflow import keras as ks
 import tensorflow as tf
 
+from yolo.ops.nms_ops import nms
+
 class CenterNetLayer(ks.Model):
   
   def __init__(self,
                max_detections=100,
-               detect_per_channel=False,
                center_thresh=0.1,
+               peak_error=1e-6,
+               class_offset=1,
+               use_nms=True,
                **kwargs):
 
     super().__init__(**kwargs)
     self._max_detections = max_detections
-    self._detect_per_channel = detect_per_channel
     self._center_thresh = center_thresh
+    self._peak_error = peak_error
+    self._class_offset = class_offset
+    self._use_nms = use_nms
   
   def process_heatmap(self, 
                       feature_map,
-                      kernel_size=3,
-                      peak_error=1e-6):
+                      kernel_size=3):
     feature_map = tf.math.sigmoid(feature_map)
     if not kernel_size or kernel_size == 1:
       feature_map_peaks = feature_map
@@ -26,7 +31,7 @@ class CenterNetLayer(ks.Model):
           feature_map, ksize=kernel_size, strides=1, padding='SAME')
 
       feature_map_peak_mask = tf.math.abs(
-          feature_map - feature_map_max_pool) < peak_error
+          feature_map - feature_map_max_pool) < self._peak_error
 
       # Zero out everything that is not a peak.
       feature_map_peaks = (
@@ -91,48 +96,22 @@ class CenterNetLayer(ks.Model):
                       feature_map_peaks,
                       batch_size,
                       width,
-                      num_channels,
-                      k=100,
-                      per_channel=False):
+                      num_classes,
+                      k=100):
 
-    if per_channel:
-      if k == 1:
-        feature_map_flattened = tf.reshape(
-            feature_map_peaks, [batch_size, -1, num_channels])
-        scores = tf.math.reduce_max(feature_map_flattened, axis=1)
-        peak_flat_indices = tf.math.argmax(
-            feature_map_flattened, axis=1, output_type=tf.dtypes.int32)
-        peak_flat_indices = tf.expand_dims(peak_flat_indices, axis=-1)
-      else:
-        # Perform top k over batch and channels.
-        feature_map_peaks_transposed = tf.transpose(feature_map_peaks,
-                                                    perm=[0, 3, 1, 2])
-        feature_map_peaks_transposed = tf.reshape(
-            feature_map_peaks_transposed, [batch_size, num_channels, -1])
-        scores, peak_flat_indices = tf.math.top_k(
-            feature_map_peaks_transposed, k=k)
-      # Convert the indices such that they represent the location in the full
-      # (flattened) feature map of size [batch, height * width * channels].
-      channel_idx = tf.range(num_channels)[tf.newaxis, :, tf.newaxis]
-      peak_flat_indices = num_channels * peak_flat_indices + channel_idx
-      scores = tf.reshape(scores, [batch_size, -1])
-      peak_flat_indices = tf.reshape(peak_flat_indices, [batch_size, -1])
-    else:
-      if k == 1:
-        feature_map_peaks_flat = tf.reshape(feature_map_peaks, [batch_size, -1])
-        scores = tf.math.reduce_max(feature_map_peaks_flat, axis=1, keepdims=True)
-        peak_flat_indices = tf.expand_dims(tf.math.argmax(
-            feature_map_peaks_flat, axis=1, output_type=tf.dtypes.int32), axis=-1)
-      else:
-        feature_map_peaks_flat = tf.reshape(feature_map_peaks, [batch_size, -1])
-        scores, peak_flat_indices = tf.math.top_k(feature_map_peaks_flat, k=k)
+    # Flatten the entire prediction per batch
+    # Now the feature_map_peaks are in shape [batch_size, num_classes * width * height]
+    feature_map_peaks_flat = tf.reshape(feature_map_peaks, [batch_size, -1])
+
+    # top_scores and top_indeces have shape [batch_size, k]
+    top_scores, top_indeces = tf.math.top_k(feature_map_peaks_flat, k=k)
 
     # Get x, y and channel indices corresponding to the top indices in the flat
     # array.
     y_indices, x_indices, channel_indices = (
-        self.get_row_col_channel_indices_from_flattened_indices(
-            peak_flat_indices, width, num_channels))
-    return scores, y_indices, x_indices, channel_indices
+      self.get_row_col_channel_indices_from_flattened_indices(
+        top_indeces, width, num_classes))
+    return top_scores, y_indices, x_indices, channel_indices
 
   def get_boxes(self, 
                 detection_scores, 
@@ -151,20 +130,26 @@ class CenterNetLayer(ks.Model):
         tf.reshape(y_indices, [-1]),
         tf.reshape(x_indices, [-1])
     ], axis=1)
+
+    # Get the heights and widths of center points
     new_height_width = tf.gather_nd(height_width_predictions, combined_indices)
     new_height_width = tf.reshape(new_height_width, [batch_size, num_boxes, -1])
+    height_width = tf.maximum(new_height_width, 0)
 
+    heights = height_width[..., 0]
+    widths = height_width[..., 1]
+
+    # Get the heights and widths of center points
     new_offsets = tf.gather_nd(offset_predictions, combined_indices)
     offsets = tf.reshape(new_offsets, [batch_size, num_boxes, -1])
+
+    y_offsets = offsets[..., 0]
+    x_offsets = offsets[..., 1]
 
     y_indices = tf.cast(y_indices, dtype=tf.float32)
     x_indices = tf.cast(x_indices, dtype=tf.float32)
 
-    height_width = tf.maximum(new_height_width, 0)
-    heights, widths = tf.split(height_width, 2, axis=-1)
-    y_offsets, x_offsets = tf.split(offsets, 2, axis=-1)
-
-    detection_classes = channel_indices
+    detection_classes = channel_indices + self._class_offset
 
     num_detections = tf.reduce_sum(tf.cast(detection_scores > 0, dtype=tf.int32), axis=1)
 
@@ -187,25 +172,26 @@ class CenterNetLayer(ks.Model):
     # Process heatmaps using 3x3 convolution and applying sigmoid
     peaks = self.process_heatmap(ct_heatmaps)
     
+    # Get top scores along with their x, y, and class
     scores, y_indices, x_indices, channel_indices = self.get_top_k_peaks(peaks, 
-                                                                         batch_size,
-                                                                         width,
-                                                                         num_channels, 
-                                                                         k=self._max_detections, 
-                                                                         per_channel=self._detect_per_channel)
+      batch_size, width, num_channels, k=self._max_detections)
 
+    # Parse the score and indeces into bounding boxes
     boxes, classes, scores, num_det = self.get_boxes(scores, 
       y_indices, x_indices, channel_indices, ct_sizes, ct_offsets, batch_size, self._max_detections)
     
+    # Normalize bounding boxes
     boxes = boxes / tf.cast(height, tf.float32)
-
+  
     boxes = tf.expand_dims(boxes, axis=-2)
     multi_class_scores = tf.gather_nd(
         peaks, tf.stack([y_indices, x_indices], -1), batch_dims=1)
 
-    boxes, scores, _, num_det = tf.image.combined_non_max_suppression(boxes=boxes, 
-      scores=multi_class_scores, max_output_size_per_class=self._max_detections, 
-      max_total_size=self._max_detections, score_threshold=0.1)
+    boxes, _, scores = nms(boxes=boxes, classes=multi_class_scores, 
+      confidence=scores, k=self._max_detections, limit_pre_thresh=True,
+      pre_nms_thresh=0.1, nms_thresh=0.4)
+
+    num_dets = tf.reduce_sum(tf.cast(scores > 0, dtype=tf.int32), axis=1)
 
     return {
       'bbox': boxes,
