40e5cf8a35265083a996d070c8d19c9927bcc2e6
David Li <davidli2881@gmail.com>
2021-03-11 21:45:07 -0500

    detection generator finished

--- centernet/modeling/layers/detection_generator.py
+++ centernet/modeling/layers/detection_generator.py
@@ -4,31 +4,88 @@ import tensorflow as tf
 from yolo.ops.nms_ops import nms
 
 class CenterNetLayer(ks.Model):
-  
+  """ CenterNet Detection Generator
+
+  Parses predictions from the CenterNet decoder into the final bounding boxes, 
+  confidences, and classes. This class contains repurposed methods from the 
+  TensorFlow Object Detection API
+  in: https://github.com/tensorflow/models/blob/master/research/object_detection/meta_architectures/center_net_meta_arch.py
+  """
   def __init__(self,
                max_detections=100,
-               center_thresh=0.1,
                peak_error=1e-6,
-               class_offset=1,
+               peak_extract_kernel_size=3,
                use_nms=True,
+               center_thresh=0.1,
+               iou_thresh=0.4,
+               class_offset=1,
                **kwargs):
+    """
+    Args:
+      max_detections: An integer specifying the maximum number of bounding
+        boxes generated. This is an upper bound, so the number of generated
+        boxes may be less than this due to thresholding/non-maximum suppression.
+      peak_error: A float for determining non-valid heatmap locations to mask.
+      peak_extract_kernel_size: An integer indicating the kernel size used when
+        performing max-pool over the heatmaps to detect valid center locations
+        from its neighbors. From the paper, set this to 3 to detect valid.
+        locations that have responses greater than its 8-connected neighbors
+      use_nms: Boolean for whether or not to use non-maximum suppression to
+        filter the bounding boxes.
+      center_thresh: A float that sets the threshold for a heatmap peak to be 
+        considered a valid detection.
+      iou_thresh: A float for the threshold IOU when filtering bounding boxes.
+      class_offset: An integer indicating to add an offset to the class 
+        prediction if the dataset labels have been shifted.
 
+    call Returns:
+      Dictionary with keys 'bbox', 'classes', 'confidence', and 'num_dets'
+      storing each bounding box in [y_min, x_min, y_max, x_max] format, 
+      its respective class and confidence score, and the number of detections
+      made.
+    """
     super().__init__(**kwargs)
+    
+    # Object center selection parameters
     self._max_detections = max_detections
-    self._center_thresh = center_thresh
     self._peak_error = peak_error
-    self._class_offset = class_offset
+    self._peak_extract_kernel_size = peak_extract_kernel_size
+
+    # Non-maximum suppression parameters
     self._use_nms = use_nms
+    self._center_thresh = center_thresh
+    self._iou_thresh = iou_thresh
+
+    self._class_offset = class_offset
   
   def process_heatmap(self, 
                       feature_map,
-                      kernel_size=3):
+                      kernel_size,
+                      center_thresh):
+    """ Processes the heatmap into peaks for box selection.
+
+    Given a heatmap, this function first masks out nearby heatmap locations of
+    the same class using max-pooling such that, ideally, only one center for the
+    object remains. Then, center locations are masked according to their scores
+    in comparison to a threshold. NOTE: Repurposed from Google OD API.
+
+    Args:
+      feature_map: A Tensor with shape [batch_size, height, width, num_classes] 
+        which is the center heatmap predictions.
+      kernel_size: An integer value for max-pool kernel size.
+      center_thresh: The threshold value for valid center location scores.
+
+    Returns:
+      A Tensor with the same shape as the input but with non-valid center
+        prediction locations masked out.
+    """
     feature_map = tf.math.sigmoid(feature_map)
     if not kernel_size or kernel_size == 1:
       feature_map_peaks = feature_map
     else:
       feature_map_max_pool = tf.nn.max_pool(
-          feature_map, ksize=kernel_size, strides=1, padding='SAME')
+          feature_map, ksize=kernel_size, 
+          strides=1, padding='SAME')
 
       feature_map_peak_mask = tf.math.abs(
           feature_map - feature_map_max_pool) < self._peak_error
@@ -37,18 +94,26 @@ class CenterNetLayer(ks.Model):
       feature_map_peaks = (
           feature_map * tf.cast(feature_map_peak_mask, tf.float32))
     
+    # Zero out peaks whose scores do not exceed threshold
+    valid_peaks_mask = feature_map_peaks > center_thresh
+    feature_map_peaks = feature_map_peaks * tf.cast(valid_peaks_mask, tf.float32)
+
     return feature_map_peaks
   
   def get_row_col_channel_indices_from_flattened_indices(self,
                                                          indices, 
                                                          num_cols,
                                                          num_channels):
-    """Computes row, column and channel indices from flattened indices.
+    """ Computes row, column and channel indices from flattened indices.
+
+    NOTE: Repurposed from Google OD API.
+
     Args:
       indices: An integer tensor of any shape holding the indices in the flattened
         space.
       num_cols: Number of columns in the image (width).
       num_channels: Number of channels in the image.
+
     Returns:
       row_indices: The row indices corresponding to each of the input indices.
         Same shape as indices.
@@ -67,14 +132,17 @@ class CenterNetLayer(ks.Model):
     return row_indices, col_indices, channel_indices
 
   def multi_range(self,
-                   limit,
-                   value_repetitions=1,
-                   range_repetitions=1,
-                   dtype=tf.int32):
-    """Creates a sequence with optional value duplication and range repetition.
+                  limit,
+                  value_repetitions=1,
+                  range_repetitions=1,
+                  dtype=tf.int32):
+    """ Creates a sequence with optional value duplication and range repetition.
+
     As an example (see the Args section for more details),
     _multi_range(limit=2, value_repetitions=3, range_repetitions=4) returns:
     [0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1]
+    NOTE: Repurposed from Google OD API.
+
     Args:
       limit: A 0-D Tensor (scalar). Upper limit of sequence, exclusive.
       value_repetitions: Integer. The number of times a value in the sequence is
@@ -82,6 +150,7 @@ class CenterNetLayer(ks.Model):
       range_repetitions: Integer. The number of times the range is repeated. With
         range_repetitions=3, the result is [0, 1, 2, .., 0, 1, 2, ..].
       dtype: The type of the elements of the resulting tensor.
+    
     Returns:
       A 1-D tensor of type `dtype` and size
         [`limit` * `value_repetitions` * `range_repetitions`] that contains the
@@ -98,19 +167,43 @@ class CenterNetLayer(ks.Model):
                       width,
                       num_classes,
                       k=100):
+    """ Gets the scores and indices of the top-k peaks from the feature map
 
+    This function flattens the feature map in order to retrieve the top-k
+    peaks, then computes the x, y, and class indices for those scores.
+    NOTE: Repurposed from Google OD API.
+
+    Args:
+      feature_map_peaks: A Tensor with shape [batch_size, height, width, num_classes] 
+        which is the processed center heatmap peaks.
+      batch_size: An integer that indicates the batch size of the input.
+      width: An integer that indicates the width (and also height) of the input.
+      num_classes: An integer for the number of possible classes. This is also
+        the channel depth of the input.
+      k: Integer that controls how many peaks to select.
+    
+    Returns:
+      top_scores: A Tensor with shape [batch_size, k] containing the top-k scores.
+      y_indices: A Tensor with shape [batch_size, k] containing the top-k 
+        y-indices corresponding to top_scores.
+      x_indices: A Tensor with shape [batch_size, k] containing the top-k 
+        x-indices corresponding to top_scores.
+      channel_indices: A Tensor with shape [batch_size, k] containing the top-k 
+        channel indices corresponding to top_scores.
+    """
     # Flatten the entire prediction per batch
     # Now the feature_map_peaks are in shape [batch_size, num_classes * width * height]
     feature_map_peaks_flat = tf.reshape(feature_map_peaks, [batch_size, -1])
 
-    # top_scores and top_indeces have shape [batch_size, k]
-    top_scores, top_indeces = tf.math.top_k(feature_map_peaks_flat, k=k)
+    # top_scores and top_indices have shape [batch_size, k]
+    top_scores, top_indices = tf.math.top_k(feature_map_peaks_flat, k=k)
 
     # Get x, y and channel indices corresponding to the top indices in the flat
     # array.
     y_indices, x_indices, channel_indices = (
       self.get_row_col_channel_indices_from_flattened_indices(
-        top_indeces, width, num_classes))
+        top_indices, width, num_classes))
+
     return top_scores, y_indices, x_indices, channel_indices
 
   def get_boxes(self, 
@@ -122,14 +215,42 @@ class CenterNetLayer(ks.Model):
                 offset_predictions,
                 batch_size,
                 num_boxes):
+    """ Organizes prediction information into the final bounding boxes.
 
+    NOTE: Repurposed from Google OD API.
+
+    Args:
+      detection_scores: A Tensor with shape [batch_size, k] containing the 
+        top-k scores.
+      y_indices: A Tensor with shape [batch_size, k] containing the top-k 
+        y-indices corresponding to top_scores.
+      x_indices: A Tensor with shape [batch_size, k] containing the top-k 
+        x-indices corresponding to top_scores.
+      channel_indices: A Tensor with shape [batch_size, k] containing the top-k 
+        channel indices corresponding to top_scores.
+      height_width_predictions: A Tensor with shape [batch_size, height, width, 2] 
+        containing the object size predictions.
+      offset_predictions: A Tensor with shape [batch_size, height, width, 2] 
+        containing the object local offset predictions.
+      batch_size: An integer for the batch size of the input.
+      num_boxes: The number of boxes.
+
+    Returns:
+      boxes: A Tensor with shape [batch_size, num_boxes, 4] that contains the
+        bounding box coordinates in [y_min, x_min, y_max, x_max] format.
+      detection_classes: A Tensor with shape [batch_size, 100] that gives the 
+        class prediction for each box.
+      detection_scores: A Tensor with shape [batch_size, 100] that gives the 
+        confidence score for each box.
+      num_detections: Number of non-zero confidence detections made.
+    """
     # TF Lite does not support tf.gather with batch_dims > 0, so we need to use
     # tf_gather_nd instead and here we prepare the indices for that.
     combined_indices = tf.stack([
         self.multi_range(batch_size, value_repetitions=num_boxes),
         tf.reshape(y_indices, [-1]),
         tf.reshape(x_indices, [-1])
-    ], axis=1)
+      ], axis=1)
 
     # Get the heights and widths of center points
     new_height_width = tf.gather_nd(height_width_predictions, combined_indices)
@@ -139,7 +260,7 @@ class CenterNetLayer(ks.Model):
     heights = height_width[..., 0]
     widths = height_width[..., 1]
 
-    # Get the heights and widths of center points
+    # Get the offsets of center points
     new_offsets = tf.gather_nd(offset_predictions, combined_indices)
     offsets = tf.reshape(new_offsets, [batch_size, num_boxes, -1])
 
@@ -161,7 +282,7 @@ class CenterNetLayer(ks.Model):
     return boxes, detection_classes, detection_scores, num_detections
 
   def call(self, inputs):
-    # Get heatmaps from decoded ourputs via final hourglass stack output
+    # Get heatmaps from decoded outputs via final hourglass stack output
     ct_heatmaps = inputs['ct_heatmaps'][-1]
     ct_sizes = inputs['ct_size'][-1]
     ct_offsets = inputs['ct_offset'][-1]
@@ -170,26 +291,30 @@ class CenterNetLayer(ks.Model):
     batch_size, height, width, num_channels = shape[0], shape[1], shape[2], shape[3]
 
     # Process heatmaps using 3x3 convolution and applying sigmoid
-    peaks = self.process_heatmap(ct_heatmaps)
+    peaks = self.process_heatmap(ct_heatmaps, 
+      kernel_size=self._peak_extract_kernel_size, 
+      center_thresh=self._center_thresh)
     
     # Get top scores along with their x, y, and class
     scores, y_indices, x_indices, channel_indices = self.get_top_k_peaks(peaks, 
       batch_size, width, num_channels, k=self._max_detections)
 
-    # Parse the score and indeces into bounding boxes
+    # Parse the score and indices into bounding boxes
     boxes, classes, scores, num_det = self.get_boxes(scores, 
       y_indices, x_indices, channel_indices, ct_sizes, ct_offsets, batch_size, self._max_detections)
     
     # Normalize bounding boxes
     boxes = boxes / tf.cast(height, tf.float32)
-  
-    boxes = tf.expand_dims(boxes, axis=-2)
-    multi_class_scores = tf.gather_nd(
-        peaks, tf.stack([y_indices, x_indices], -1), batch_dims=1)
 
-    boxes, _, scores = nms(boxes=boxes, classes=multi_class_scores, 
-      confidence=scores, k=self._max_detections, limit_pre_thresh=True,
-      pre_nms_thresh=0.1, nms_thresh=0.4)
+    # Apply nms 
+    if self._use_nms:
+      boxes = tf.expand_dims(boxes, axis=-2)
+      multi_class_scores = tf.gather_nd(peaks, 
+        tf.stack([y_indices, x_indices], -1), batch_dims=1)
+
+      boxes, _, scores = nms(boxes=boxes, classes=multi_class_scores, 
+        confidence=scores, k=self._max_detections, limit_pre_thresh=True,
+        pre_nms_thresh=self._center_thresh, nms_thresh=self._iou_thresh)
 
     num_dets = tf.reduce_sum(tf.cast(scores > 0, dtype=tf.int32), axis=1)
 
