46ad107843612507fb0dcff714b3fddbd45e254a
David Li <davidli2881@gmail.com>
2021-04-13 15:43:40 +0000

    tpu stuff

--- centernet/modeling/CenterNet.py
+++ centernet/modeling/CenterNet.py
@@ -94,7 +94,7 @@ def build_centernet(input_specs, task_config, l2_regularization):
   model = CenterNet(backbone=backbone, decoder=decoder, head=head, filter=filter)
 
 
-  # test_input = tf.zeros(shape=(1, 512, 512, 3), dtype=tf.float32)
+  # test_input = tf.zeros(shape=(1, 512, 512, 3), dtype=tf.bfloat16)
   # model(test_input)
   model.build(input_specs.shape)
   model.summary()
--- centernet/train.py
+++ centernet/train.py
@@ -54,7 +54,9 @@ nohup python3.8 -m yolo.train --mode=train_and_eval --experiment=yolo_subdiv_cus
 nohup python3 -m yolo.train --mode=train_and_eval --experiment=yolo_custom --model_dir=../checkpoints/yolov4- --config_file=yolo/configs/experiments/yolov4-1gpu.yaml  >> yolov4-1gpu.log & tail -f yolov4-1gpu.log
 nohup python3.8 -m yolo.train --mode=train_and_eval --experiment=yolo_custom --model_dir=../checkpoints/yolov3-1gpu_mosaic --config_file=yolo/configs/experiments/yolov3-1gpu.yaml  >> yolov3-1gpu.log & tail -f yolov3-1gpu.log
 evalaute CenterNet:
-nohup python -m centernet.train --mode=train_and_eval --experiment=centernet_custom --model_dir=../checkpoints/centernet- --config_file=centernet/configs/experiments/centernet-eval.yaml  >> centernet-eval.log & tail -f centernet-eval.log
+nohup python -m centernet.train --mode=train_and_eval --experiment=centernet_custom --model_dir=../checkpoints/centernet- --config_file=centernet/configs/experiments/centernet-eval-tpu.yaml  >> centernet-eval.log & tail -f centernet-eval.log
+evalaute CenterNet on TPU:
+nohup python -m centernet.train --mode=eval --tpu=node-8 --experiment=centernet_tpu --model_dir=../checkpoints/centernet- --config_file=centernet/configs/experiments/centernet-eval-tpu.yaml  >> centernet-eval.log & tail -f centernet-eval.log
 """
 
 
