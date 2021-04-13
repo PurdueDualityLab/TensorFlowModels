c7098d052c737ab9566055ce44e68d66552b56f2
David Li <davidli2881@gmail.com>
2021-04-13 15:57:57 +0000

    getting cannot build error

--- centernet/modeling/CenterNet.py
+++ centernet/modeling/CenterNet.py
@@ -93,7 +93,6 @@ def build_centernet(input_specs, task_config, l2_regularization):
 
   model = CenterNet(backbone=backbone, decoder=decoder, head=head, filter=filter)
 
-
   # test_input = tf.zeros(shape=(1, 512, 512, 3), dtype=tf.bfloat16)
   # model(test_input)
   model.build(input_specs.shape)
