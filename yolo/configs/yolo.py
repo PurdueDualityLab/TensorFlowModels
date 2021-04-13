1a0950aec6462519aca8dcd3d2baab6ae2d817a8
vishnubanna <banna3vishnu@gmail.com>
2021-04-13 02:50:55 -0400

    torch loss scaling

--- yolo/configs/yolo.py
+++ yolo/configs/yolo.py
@@ -149,7 +149,7 @@ class Mosaic(hyperparams.Config):
   crop_area: List[int] = dataclasses.field(default_factory=lambda: [0.25, 1.0])
   crop_area_mosaic: List[int] = dataclasses.field(
       default_factory=lambda: [0.25, 0.95])
-  random_crop: bool = True
+  random_crop: bool = False
   random_crop_mosaic: bool = True
 
 
@@ -164,16 +164,16 @@ class Parser(hyperparams.Config):
   random_flip: bool = True
   pct_rand: float = 0.0
   jitter_im: float = 0.6
-  jitter_boxes: float = 0.2
-  aug_rand_translate: float = 0.05
-  aug_rand_saturation: float = 0.7  #1.5
-  aug_rand_brightness: float = 0.6  #1.5
+  jitter_boxes: float = 0.4
+  aug_rand_translate: float = 0.1
+  aug_rand_saturation: float = 0.75 #1.5
+  aug_rand_brightness: float = 0.75 #1.5
   aug_rand_hue: float = 0.1  #0.015
   aug_rand_zoom: float = 0.5
   aug_rand_angle: float = 7.0
   use_tie_breaker: bool = True
   use_scale_xy: bool = True
-  anchor_thresh: float = 4.0
+  anchor_thresh: float = 0.213
   mosaic: Mosaic = Mosaic()
 
 
