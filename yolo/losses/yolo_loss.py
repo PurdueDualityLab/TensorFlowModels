499be6a18a2a4c1c643ad4f38494c803379672a7
vishnubanna <banna3vishnu@gmail.com>
2021-04-29 20:39:45 -0400

    almost_fin_test

--- yolo/losses/yolo_loss.py
+++ yolo/losses/yolo_loss.py
@@ -205,8 +205,8 @@ def new_coord_scale_boxes(pred_xy, pred_wh, width, height, anchor_grid,
                           grid_points, max_delta, scale_xy):
   scale_xy = tf.cast(scale_xy, pred_xy.dtype)
 
-  # pred_xy = tf.math.sigmoid(pred_xy)
-  # pred_wh = tf.math.sigmoid(pred_wh)
+  pred_xy = tf.math.sigmoid(pred_xy)
+  pred_wh = tf.math.sigmoid(pred_wh)
   pred_xy = pred_xy * scale_xy - 0.5 * (scale_xy - 1)
   scaler = tf.convert_to_tensor([width, height])
   box_xy = grid_points + pred_xy / scaler
@@ -258,12 +258,11 @@ def get_predicted_box_newcords(width,
                                darknet=False,
                                max_delta=5.0,
                                normalizer=1.0):
-  # pred_xy = tf.math.sigmoid(unscaled_box[..., 0:2])
-  # pred_wh = tf.math.sigmoid(unscaled_box[..., 2:4])
+
   pred_xy = unscaled_box[..., 0:2]
   pred_wh = unscaled_box[..., 2:4]
-  pred_xy = tf.math.sigmoid(pred_xy)
-  pred_wh = tf.math.sigmoid(pred_wh)
+  # pred_xy = tf.math.sigmoid(pred_xy)
+  # pred_wh = tf.math.sigmoid(pred_wh)
 
   if darknet:
     # box_xy, box_wh, pred_box = darknet_new_coord_boxes(pred_xy, pred_wh, width, height, anchor_grid, grid_points, max_delta, scale_xy)
@@ -726,10 +725,10 @@ class Yolo_Loss(object):
     # 0. if smoothign is used, they prop the gradient of the sigmoid first
     #    but the sigmoid, if it is not enabled, they do not use the gradient of
     #    the sigmoid
-    # if self._new_cords:
-    #   # if smoothing is enabled they for some reason
-    #   # take the sigmoid many times
-    #   y_pred = grad_sigmoid(y_pred)
+    if self._new_cords:
+      # if smoothing is enabled they for some reason
+      # take the sigmoid many times
+      y_pred = grad_sigmoid(y_pred)
 
     # 1. generate and store constants and format output
     shape = tf.shape(true_counts)
