d7a4a46c081a9fe86a5db2e7fd01acc64265fd55
Vishnu Banna <banna3vishnu@gmail.com>
2021-06-09 00:51:01 -0700

    keep thresh potential revert

--- yolo/configs/yolo.py
+++ yolo/configs/yolo.py
@@ -166,8 +166,8 @@ class Parser(hyperparams.Config):
   aug_rand_saturation: float = 1.5
   aug_rand_brightness: float = 1.5
   aug_rand_hue: float = 0.1
-  aug_scale_min: float = 0.1
-  aug_scale_max: float = 2.0
+  aug_scale_min: float = 0.5
+  aug_scale_max: float = 1.5
   use_tie_breaker: bool = True
   use_scale_xy: bool = True
   anchor_thresh: float = 0.2
