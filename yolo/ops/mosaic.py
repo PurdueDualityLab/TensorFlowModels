e84c29dad6a68eb87810034e7e08dd0f166cdf87
Vishnu Banna <banna3vishnu@gmail.com>
2021-06-20 20:19:32 -0700

    mosaic op

--- yolo/ops/mosaic.py
+++ yolo/ops/mosaic.py
@@ -150,10 +150,12 @@ class Mosaic(object):
 
   def _mosaic_crop_image(self, image, boxes, classes, is_crowd, area, crop_area,
                          width, height):
+
+                         
     if self._mosaic_crop_mode == "scale":
       image, infos = preprocessing_ops.resize_and_jitter_image(
-          image, [self._output_size[0], self._output_size[1]],
-          [self._output_size[0], self._output_size[1]],
+          image, [self._output_size[0] * 2, self._output_size[1] * 2],
+          [self._output_size[0] * 2, self._output_size[1] * 2],
           letter_box=False,
           aug_scale_min=crop_area[0],
           aug_scale_max=crop_area[1],
@@ -169,9 +171,9 @@ class Mosaic(object):
           seed=self._seed)
       infos = [info]
       image, infos_ = preprocessing_ops.resize_and_jitter_image(
-          image, [self._output_size[0], self._output_size[1]],
-          [self._output_size[0], self._output_size[1]],
-          letter_box=True,
+          image, [self._output_size[0] * 2, self._output_size[1] * 2],
+          [self._output_size[0] * 2, self._output_size[1] * 2],
+          letter_box=None,
           aug_scale_min=crop_area[0],
           aug_scale_max=crop_area[1],
           jitter=0.0,
@@ -334,7 +336,7 @@ class Mosaic(object):
         area = tf.concat(areas, axis=0)
 
         height, width = self._output_size[0], self._output_size[1]
-        image = tf.image.resize(image, (height, width))
+        # image = tf.image.resize(image, (height, width))
 
         if self._mosaic_crop_mode is not None:
           image, boxes, classes, is_crowd, area, info = self._mosaic_crop_image(
