87795ff7d241e5e7fcc1b851ce249602ddcfc4a5
Vishnu Banna <banna3vishnu@gmail.com>
2021-06-21 19:26:38 -0700

    utils

--- yolo/configs/yolo.py
+++ yolo/configs/yolo.py
@@ -126,7 +126,7 @@ class Mosaic(hyperparams.Config):
   crop_area: List[int] = dataclasses.field(default_factory=lambda: [0.45, 1.0])
   crop_area_mosaic: List[int] = dataclasses.field(
       default_factory=lambda: [0.4, 1.6])
-  aspect_ratio_mode: str = 'distort'
+  aspect_ratio_mode: str = 'letter'
   mosaic_crop_mode: Optional[str] = 'crop_scale'
   aug_scale_min: Optional[float] = 0.4
   aug_scale_max: Optional[float] = None
