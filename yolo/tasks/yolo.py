b68c58e5e12c14b9ba57dbfc1e82aad267eb31ec
Vishnu Banna <banna3vishnu@gmail.com>
2021-08-03 02:04:27 -0700

    prproc

--- yolo/tasks/yolo.py
+++ yolo/tasks/yolo.py
@@ -313,7 +313,7 @@ class YoloTask(base_task.Task):
     # get the data point
     image, label = inputs
 
-    p = num_replicas = tf.distribute.get_strategy().num_replicas_in_sync
+    num_replicas = tf.distribute.get_strategy().num_replicas_in_sync
     if self._task_config.model.filter.use_scaled_loss:
       num_replicas = 1
       
