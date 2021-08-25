647ac588480c26fb982bb9e54fcd56d5e8e78afc
Vishnu Banna <banna3vishnu@gmail.com>
2021-08-24 20:30:48 -0400

    debug exp

--- yolo/dataloaders/yolo_input_test.py
+++ yolo/dataloaders/yolo_input_test.py
@@ -31,13 +31,8 @@ prep_gpu()
 def test_yolo_input_task():
   # with tf.device('/CPU:0'):
   experiment = "yolo_custom"
-  # config_path = ["yolo/configs/experiments/yolov4/tpu/512.yaml"]
-  # config_path = ["yolo/configs/experiments/yolov4/tpu/512-extra-boxes.yaml"]
-  # config_path = ["yolo/configs/experiments/yolov4-csp/tpu/640.yaml"]
-  # config_path = ["yolo/configs/experiments/yolov4-csp/debug/640-3x-64-cstm.yaml"]
-  # config_path = ["yolo/configs/experiments/yolov4-csp/debug/640-baseline-ema-rcrop.yaml"]
-  # config_path = ["yolo/configs/experiments/yolov4-csp/debug/640-large-base-fntn.yaml"]
-  config_path = ["yolo/configs/experiments/yolov4-csp/debug/640-3x-64-wd.yaml"]
+  config_path = ["yolo/configs/experiments/yolov4/tpu/512.yaml"]
+  # config_path = ["yolo/configs/experiments/yolov4-csp/debug/640-3x-64-wd.yaml"]
 
   config = train_utils.ParseConfigOptions(
       experiment=experiment, config_file=config_path)
