25387d5d8c9ba925970be4194264a368e990ace6
Vishnu Banna <banna3vishnu@gmail.com>
2021-08-27 18:13:15 -0400

    hsv

--- yolo/losses/yolo_loss.py
+++ yolo/losses/yolo_loss.py
@@ -456,8 +456,8 @@ class Yolo_Loss(object):
     if self._loss_type == 1:
       iou, liou = box_ops.compute_giou(true_box, pred_box)
     elif self._loss_type == 2:
-      iou, liou = box_ops.compute_ciou(true_box, pred_box, darknet=darknet)
-      # iou = liou = box_ops.bbox_iou(true_box, pred_box, x1y1x2y2 = False, CIoU=True)
+      # iou, liou = box_ops.compute_ciou(true_box, pred_box, darknet=darknet)
+      iou = liou = box_ops.bbox_iou(true_box, pred_box, x1y1x2y2 = False, CIoU=True)
     else:
       iou = box_ops.compute_iou(true_box, pred_box)
       liou = iou
