0fbbf37e74cc29d10eb8c6e70f7a2c2069acf54b
Vishnu Banna <banna3vishnu@gmail.com>
2021-08-29 14:15:42 -0400

    anchor free

--- yolo/dataloaders/yolo_input_test.py
+++ yolo/dataloaders/yolo_input_test.py
@@ -39,7 +39,7 @@ def test_yolo_input_task():
   # config_path = ["yolo/configs/experiments/yolov4-csp/debug/640-large-base-fntn.yaml"]
   # config_path = ["yolo/configs/experiments/yolov4-csp/debug/640-3x-64-wd.yaml"]
   # config_path = ["yolo/configs/experiments/yolov4-csp/debug/640.yaml"]
-  config_path = ["yolo/configs/experiments/yolov4-csp-anchor-free/tpu/640.yaml"]
+  config_path = ["yolo/configs/experiments/yolov4-csp-anchor-free/debug/640.yaml"]
 
   config = train_utils.ParseConfigOptions(
       experiment=experiment, config_file=config_path)
