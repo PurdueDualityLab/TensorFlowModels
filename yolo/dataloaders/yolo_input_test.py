7cd6020876119eb34b5fc3430379df7450b1489e
Vishnu Banna <banna3vishnu@gmail.com>
2021-09-05 17:41:15 -0400

    exp a

--- yolo/dataloaders/yolo_input_test.py
+++ yolo/dataloaders/yolo_input_test.py
@@ -38,7 +38,8 @@ def test_yolo_input_task():
   # config_path = ["yolo/configs/experiments/yolov4-csp/debug/640-baseline-ema-rcrop.yaml"]
   # config_path = ["yolo/configs/experiments/yolov4-csp/debug/640-large-base-fntn.yaml"]
   # config_path = ["yolo/configs/experiments/yolov4-csp/debug/640-3x-64-wd.yaml"]
-  config_path = ["yolo/configs/experiments/yolov4-csp/debug/hsv/640-hsv.yaml"]
+  # config_path = ["yolo/configs/experiments/yolov4-csp/debug/hsv/640-hsv.yaml"]
+  config_path = ["yolo/configs/experiments/yolov4-csp/debug/alt-cfg/640-e1.yaml"]
   # config_path = ["yolo/configs/experiments/yolov4-csp-anchor-free/debug/640.yaml"]
 
   config = train_utils.ParseConfigOptions(
