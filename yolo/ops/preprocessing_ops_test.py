17abbfd1a685bafdf1410f229b5ef9a061073b7a
akhil-chinnakotla <achinnak@purdue.edu>
2021-09-09 19:12:25 -0400

    Minor: Image Shape Tests

--- yolo/ops/preprocessing_ops_test.py
+++ yolo/ops/preprocessing_ops_test.py
@@ -6,40 +6,6 @@ from yolo.ops import preprocessing_ops
 
 
 class InputUtilsTest(parameterized.TestCase, tf.test.TestCase):
-
-  @parameterized.parameters((416, 416, 5, 300, 300), (100, 200, 6, 50, 50))
-  def testResizeCropFilter(self, default_width, default_height, num_boxes,
-                           target_width, target_height):
-    image = tf.convert_to_tensor(
-        np.random.rand(default_width, default_height, 3))
-    boxes = tf.convert_to_tensor(np.random.rand(num_boxes, 4))
-    resized_image, resized_boxes = preprocessing_ops.resize_crop_filter(
-        image, boxes, default_width, default_height, target_width,
-        target_height)
-    resized_image_shape = tf.shape(resized_image)
-    resized_boxes_shape = tf.shape(resized_boxes)
-    self.assertAllEqual([default_height, default_width, 3],
-                        resized_image_shape.numpy())
-    self.assertAllEqual([num_boxes, 4], resized_boxes_shape.numpy())
-
-  @parameterized.parameters((7, 7., 5.), (25, 35., 45.))
-  def testTranslateBoxes(self, num_boxes, translate_x, translate_y):
-    boxes = tf.convert_to_tensor(np.random.rand(num_boxes, 4))
-    translated_boxes = preprocessing_ops.translate_boxes(
-        boxes, translate_x, translate_y)
-    translated_boxes_shape = tf.shape(translated_boxes)
-    self.assertAllEqual([num_boxes, 4], translated_boxes_shape.numpy())
-
-  @parameterized.parameters((100, 200, 75., 25.), (400, 600, 25., 75.))
-  def testTranslateImage(self, image_height, image_width, translate_x,
-                         translate_y):
-    image = tf.convert_to_tensor(np.random.rand(image_height, image_width, 4))
-    translated_image = preprocessing_ops.translate_image(
-        image, translate_x, translate_y)
-    translated_image_shape = tf.shape(translated_image)
-    self.assertAllEqual([image_height, image_width, 4],
-                        translated_image_shape.numpy())
-
   @parameterized.parameters(([1, 2], 20, 0), ([13, 2, 4], 15, 0))
   def testPadMaxInstances(self,
                           input_shape,
@@ -51,6 +17,76 @@ class InputUtilsTest(parameterized.TestCase, tf.test.TestCase):
         np.ones(input_shape), instances, pad_axis=pad_axis)
     self.assertAllEqual(expected_output_shape,
                         tf.shape(output).numpy())
+
+  @parameterized.parameters((100, 200))
+  def testGetImageShape(self, image_height, image_width):
+    image = tf.convert_to_tensor(np.random.rand(image_height, image_width, 3))
+    image_shape = preprocessing_ops.get_image_shape(image)
+    self.assertAllEqual((image_height, image_width),
+                        image_shape)
+
+  @parameterized.parameters((400, 600, .5, .5, .0, True), (100, 200, .5, .5, .5))
+  def testImageRandHSV(self,
+                       image_height,
+                       image_width,
+                       rh,
+                       rs,
+                       rv,
+                       is_darknet = False):
+    image = tf.convert_to_tensor(np.random.rand(image_height, image_width, 3))
+    processed_image = preprocessing_ops.image_rand_hsv(image,
+                                                       rh,
+                                                       rs,
+                                                       rv,
+                                                       darknet= is_darknet)
+    processed_image_shape = tf.shape(processed_image)
+    self.assertAllEqual([image_height, image_width, 3],
+                        processed_image_shape.numpy())
+
+  @parameterized.parameters((100, 200, 50, 50), (100, 200, 50, 50, 0.5))
+  def testRandomWindowCrop(self,
+                           image_height,
+                           image_width,
+                           target_height,
+                           target_width,
+                           translate=0.0):
+    image = tf.convert_to_tensor(np.random.rand(image_height, image_width, 3))
+    processed_image, _ = preprocessing_ops.random_window_crop(image,
+                                                              target_height,
+                                                              target_width,
+                                                              translate)
+    processed_image_shape = tf.shape(processed_image)
+    self.assertAllEqual([target_height, target_width, 3],
+                        processed_image_shape.numpy())
+
+  @parameterized.parameters((100, 200, [50, 100]))
+  def testResizeAndJitterImage(self,
+                               image_height,
+                               image_width,
+                               desired_size):
+    image = tf.convert_to_tensor(np.random.rand(image_height, image_width, 3))
+    processed_image, _, _ = preprocessing_ops.resize_and_jitter_image(image, desired_size)
+    processed_image_shape = tf.shape(processed_image)
+    self.assertAllEqual([desired_size[0], desired_size[1], 3],
+                        processed_image_shape.numpy())
+
+  @parameterized.parameters((400, 600, [200, 300]))
+  def testAffineWarpImage(self,
+                          image_height,
+                          image_width,
+                          desired_size,
+                          degrees=7.0,
+                          scale_min=0.1,
+                          scale_max=1.9):
+    image = tf.convert_to_tensor(np.random.rand(image_height, image_width, 3))
+    processed_image, _, _ = preprocessing_ops.affine_warp_image(image,
+                                                                desired_size,
+                                                                degrees=degrees,
+                                                                scale_min=scale_min,
+                                                                scale_max=scale_max)
+    processed_image_shape = tf.shape(processed_image)
+    self.assertAllEqual([desired_size[0], desired_size[1], 3],
+                        processed_image_shape.numpy())
 
 
 if __name__ == '__main__':
