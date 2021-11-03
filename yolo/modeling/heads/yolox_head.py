126e7436141c5fe74be80c50c01dce9855ad8689
Wenxin Jiang <wenxinjiang@pal-nat186-75-47.itap.purdue.edu>
2021-11-03 13:33:33 -0400

    add DWConv, biases initialization and call

--- yolo/modeling/heads/yolox_head.py
+++ yolo/modeling/heads/yolox_head.py
@@ -16,8 +16,8 @@
 """Yolox heads."""
 
 import tensorflow as tf
-# from keras.models import Sequential
-# from keras.layers import Conv2D, DepthwiseConv2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, DepthwiseConv2D, Concatenate
 from torch.nn.modules.conv import Conv2d
 
 from yolo.modeling.layers import nn_blocks
@@ -56,11 +56,12 @@ class YOLOXHead(tf.keras.layers.Layer):
     self._cls_pred = []
     self._reg_preds = []
     self._obj_preds = []
-
     self._stems = []
-    # TODO if there is no BaseConv, we need to define that.
-    # Conv = tf.keras.Sequential()
-    # Conv = DepthwiseConv2D if depthwise else BaseConv 
+
+    self._prior_prob = 1e-2
+    self.bias=-tf.math.log((1-self.prior_prob)/self.prior_prob)
+
+    Conv = DWConv if depthwise else BaseConv 
     for i in range(len(in_channels)):
       self._stems.append(
         nn_blocks.ConvBN(
@@ -74,47 +75,52 @@ class YOLOXHead(tf.keras.layers.Layer):
       )
 
       self._cls_conv.append (
-        nn_blocks.ConvBN(
-          filters=int(256 * width),
-          kernel_size=3,
-          strides=(1, 1),
-          padding='same', # TODO (pad = ksize - 1 ) // 2
-          use_bn = True,
-          activation=act,
-        ),
-      )
-      self._cls_conv.append(
-        nn_blocks.ConvBN(
-          filters=int(256 * width),
-          kernel_size=3,
-          strides=(1, 1),
-          padding='same', # TODO
-          use_bn = True,
-          activation=act,
-        ),
+        Sequential(
+          [
+          nn_blocks.ConvBN(
+            filters=int(256 * width),
+            kernel_size=3,
+            strides=(1, 1),
+            padding='same', # TODO (pad = ksize - 1 ) // 2
+            use_bn = True,
+            activation=act,
+          ),
+           nn_blocks.ConvBN(
+            filters=int(256 * width),
+            kernel_size=3,
+            strides=(1, 1),
+            padding='same', # TODO
+            use_bn = True,
+            activation=act,
+          ),
+          ]
+        )
       )
     
 
       self._reg_conv.append(
-        nn_blocks.ConvBN(
-          filters=int(256 * width),
-          kernel_size=3,
-          strides=(1, 1),
-          padding='same', # TODO
-          use_bn = True,
-          activation=act,
-        ),
+        Sequential(
+          [
+            nn_blocks.ConvBN(
+            filters=int(256 * width),
+            kernel_size=3,
+            strides=(1, 1),
+            padding='valid', # TODO
+            use_bn = True,
+            activation=act,
+          ),
+          nn_blocks.ConvBN(
+            filters=int(256 * width),
+            kernel_size=3,
+            strides=(1, 1),
+            padding='valid', # TODO
+            use_bn = True,
+            activation=act,
+          ),
+          ]
+        )
       )
-      self._reg_conv.append(
-        nn_blocks.ConvBN(
-          filters=int(256 * width),
-          kernel_size=3,
-          strides=(1, 1),
-          padding='same', # TODO
-          use_bn = True,
-          activation=act,
-        ),
-      )
+        
 
 
       self._cls_pred.append(
@@ -123,6 +129,7 @@ class YOLOXHead(tf.keras.layers.Layer):
           kernel_size=1,
           strides=(1, 1),
           padding='valid',
+          bias_initializer=tf.keras.initializers.constant(self.bias)
         ),
       )
       self._reg_preds.append(
@@ -139,88 +146,74 @@ class YOLOXHead(tf.keras.layers.Layer):
           kernel_size=1,
           strides=(1, 1),
           padding='valid',
+          bias_initializer=tf.keras.initializers.constant(self.bias)
         ),
       )
       
-      self.use_l1 = False
-      self.l1_loss = tf.keras.losses.MAE # TODO need reduce_mean after the loss
-      self.bcewithlog_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-      self.iou_loss = box_ops.compute_iou
-      self.strides = strides
-      self.grids = [tf.zeros(1)] * len(in_channels)
-    #def __initialize_biases(self, prior_prob):
-        #for conv in self._cls_preds:
-            #b = conv.bias.(self._n_anchors, -1)
-            #b.data.fill_(-math.log((1 - prior_prob) / prior_prob))
-            #conv.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
-#
-        #for conv in self._obj_preds:
-            #b = conv.bias.view(self._n_anchors, -1)
-            #b.data.fill_(-math.log((1 - prior_prob) / prior_prob))
-            #conv.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
-    #def build(self, xin, lables=None, imgs=None):
-    #self._head = dict()
-    #for key in self._key_list:
-    #  scale = 2**int(key)
-    #  self._head[key] = nn_blocks.ConvBN(
-    #      bias_initializer=self.bias_init(scale, input_shape[key][-1]),
-    #      **self._conv_config)
+      # self.use_l1 = False
+      # self.l1_loss = tf.keras.losses.MAE # TODO need reduce_mean after the loss
+      # self.bcewithlog_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+      # self.iou_loss = box_ops.compute_iou
+      # self.strides = strides
+      # self.grids = [tf.zeros(1)] * len(in_channels)
 
-
-
-
-  '''
     def call(self, xin, labels=None, imgs=None):
-    outputs = []  
-    origin_preds = []
-    x_shifts = []
-    y_shifts = []
-    expanded_strides = []
-    for k, (cls_conv, reg_conv, stride_this_level, x) in enumerate(
-        zip(self._cls_convs self._reg_conv, self.strides, xin)
-    ):
-        x = self.stems[k](x)
-        cls_x = x
-        reg_x = x
+      outputs = []
+      origin_preds = []
+      x_shifts = []
+      y_shifts = []
+      expanded_strides = []
 
-        cls_feat = cls_conv(cls_x)
-        cls_output = self._cls_preds[k](cls_feat)
+      for k, (cls_conv, reg_conv, stride_this_level, x) in enumerate(
+          zip(self.cls_convs, self.reg_convs, self.strides, xin)
+      ):
+          x = self.stems[k](x)
+          cls_x = x
+          reg_x = x
 
-        reg_feat = reg_conv(reg_x)
-        reg_output = self._reg_preds[k](reg_feat)
-        obj_output = self._obj_preds[k](reg_feat)
-        if self.training:
-          output = tf.concat([reg_output, obj_output, cls_output], 1)
-          output, grid = self.get_output_and_grid(
-              output, k, stride_this_level, xin[0].type()
-          )
-          x_shifts.append(grid[:, :, 0])
-          y_shifts.append(grid[:, :, 1])
-          expanded_strides.append(
-              tf.zeros(1, grid.shape[1])
-              .fill_(stride_this_level)
-              .type_as(xin[0])
-          )
-                if self.use_l1:
-                    batch_size = reg_output.shape[0]
-                    hsize, wsize = reg_output.shape[-2:]
-                    reg_output = reg_output.view(
-                        batch_size, self.n_anchors, 4, hsize, wsize
-                    )
-                    reg_output = reg_output.permute(0, 1, 3, 4, 2).reshape( 
-                    # reg_output: batch_size * n_anchors * 4 * height * width
-                    # permute: batch_size * n_anchors * height * width * 4
-                    # reshape: [batch_size, N_anchors height width, 4]
-                    # This is also called "origin_pred"
-                        batch_size, -1, 4
-                    )
-                    origin_preds.append(reg_output.clone())
+# cls - feature and output
+          cls_feat = cls_conv(cls_x)
+          cls_output = self.cls_preds[k](cls_feat)
 
-            else:   # not training
-                output = tf.concat(
-                    [reg_output, obj_output.sigmoid(), cls_output.sigmoid()], 1
-                )
+# reg & obj - feature and output
+          reg_feat = reg_conv(reg_x)
+          reg_output = self.reg_preds[k](reg_feat)
+          obj_output = self.obj_preds[k](reg_feat)
+          output=Concatenate(-1)([reg_output,obj_output,cls_output])
+          outputs.append(output)
+      return outputs
+          # if self.training:
+          #     output = tf.concat([reg_output, obj_output, cls_output], 1)
+          #     output, grid = self.get_output_and_grid(
+          #         output, k, stride_this_level, xin[0].type()
+          #     )
+          #     x_shifts.append(grid[:, :, 0])
+          #     y_shifts.append(grid[:, :, 1])
+          #     expanded_strides.append(
+          #         tf.zeros(1, grid.shape[1])
+          #         .fill_(stride_this_level)
+          #         .type_as(xin[0])
+          #     )
+          #     if self.use_l1:
+          #         batch_size = reg_output.shape[0]
+          #         hsize, wsize = reg_output.shape[-2:]
+          #         reg_output = reg_output.view(
+          #             batch_size, self.n_anchors, 4, hsize, wsize
+          #         )
+          #         reg_output = reg_output.permute(0, 1, 3, 4, 2).reshape( 
+          #         # reg_output: batch_size * n_anchors * 4 * height * width
+          #         # permute: batch_size * n_anchors * height * width * 4
+          #         # reshape: [batch_size, N_anchors height width, 4]
+          #         # This is also called "origin_pred"
+          #             batch_size, -1, 4
+          #         )
+          #         origin_preds.append(reg_output.clone())
 
-            outputs.append(output)
-   return outputs
-  '''
+          # else:   # not training
+          #     output = tf.concat(
+          #         [reg_output, obj_output.sigmoid(), cls_output.sigmoid()], 1
+          #     )
+
+          # outputs.append(output)
+          # return outputs
+    
